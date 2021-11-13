7ad855d2b0b67714eed5b26461d8852cb2a788be
ZigerFrigg <91021992+ZigerFrigg@users.noreply.github.com>
2021-11-13 12:53:07 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to the TFKS<br></br>
+          <b>T</b>ennis <b>F</b>erien <b>K</b>rypto <b>S</b>parschwein
+        </p>
+        <img src="logo.png" width="350" />
+        <a
+          className="App-link"
+          href="https://coingecko.com/en"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Coingecko
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
